8bda7950d0f2a8ae40e06677915ad9eb35f21553
LopsanAMO <makiki789@gmail.com>
2017-12-02 17:40:01 -0600

    updating urls

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,21 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 from api.api.v1 import urls as ApiUrls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(ApiUrls))
+]
+
+urlpatterns += [
+    url(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {
+            'document_root': settings.MEDIA_ROOT,
+        }
+    )
 ]
