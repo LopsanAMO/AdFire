37ea2679264463a667bd9876c1b6a04f97e4f8bf
LopsanAMO <makiki789@gmail.com>
2017-12-02 16:50:02 -0600

    adding static root

--- config/settings/base.py
+++ config/settings/base.py
@@ -110,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
 
 
 FIREBASE_API_KEY = 'AIzaSyCUESYlpQYbZY4skk_TuVuvOBWBCAduGOk'
